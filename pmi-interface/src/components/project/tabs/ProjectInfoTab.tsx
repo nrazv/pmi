8ac222aa24bcbfb839b2ba2d20e143d5a315b782
zer0 <peter00nr@outlook.com>
2025-01-05 21:10:16 +0100

    ProjectInfoTab update

--- pmi-interface/src/components/project/tabs/ProjectInfoTab.tsx
+++ pmi-interface/src/components/project/tabs/ProjectInfoTab.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import TabPanelProps from "./TabPanelProps";
+import { Box } from "@mui/material";
 
-function ProjectInfoTab() {
-  return <div>ProjectInfoTab</div>;
+function ProjectInfoTab(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
 }
 
 export default ProjectInfoTab;
